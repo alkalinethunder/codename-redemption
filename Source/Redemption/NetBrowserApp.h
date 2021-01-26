ea5791cc342b10eeca2896cc7ce3a0f88618e684
Michael VanOverbeek <theplexgate@gmail.com>
2021-01-25 23:14:34 -0500

    netbrowser base

---
+++ Source/Redemption/NetBrowserApp.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AppWidget.h"
+#include "Components/Button.h"
+#include "Components/EditableText.h"
+#include "Components/ScrollBox.h"
+
+
+#include "NetBrowserApp.generated.h"
+
+UCLASS(BlueprintType, Blueprintable, Abstract)
+class REDEMPTION_API UNetBrowserApp : public UAppWidget
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	UEditableText* AddressBar;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	UButton* GoButton;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	UButton* BackButton;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	UButton* ForwardButton;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	UScrollBox* WebContent;
+};
